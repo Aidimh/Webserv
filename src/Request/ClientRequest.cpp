49fdaeff84a1de0155480e9ef405eff9b112cb2c
mzanana <mr.zananamohamed@gmail.com>
2026-08-06 22:16:51 +0100

    fix headers part

--- src/Request/ClientRequest.cpp
+++ src/Request/ClientRequest.cpp
@@ -339,7 +339,8 @@ void ClientRequest::parse(Client& client)
 {
     if (this->state == ERROR_STATE)
 		return;
-	
+	if (this->state != HEADERS)
+		return;
 	size_t begin = removeWhitespace(client);
 	if (begin > 0)
 		client.request.erase(0, begin);
